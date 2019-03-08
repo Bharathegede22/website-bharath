422c53e158f31fc3f1c2ccfe10e12be7104ef7c4
Hegde <bhegde@lowes.com>
2019-03-08 18:34:35 +0530

    gatsby files removed

--- component---src-pages-page-2-js-eb888d24b8a081cd5280.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0xc6c285f8fd10],{200:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=l(4),n=u(a),f=l(63),o=u(f),c=function(){return n.default.createElement("div",null,n.default.createElement("h1",null,"Hi from the second page"),n.default.createElement("p",null,"Welcome to page 2"),n.default.createElement(o.default,{to:"/page-3"},"Go to page 3"))};t.default=c,e.exports=t.default}});
-//# sourceMappingURL=component---src-pages-page-2-js-eb888d24b8a081cd5280.js.map
